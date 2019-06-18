2d20f02dd2ee5b01fc758056e728580e26b210ce
Rickard Åberg <rickard.aaberg@gmail.com>
2019-06-18 13:24:36 +0200

    fetches books from api - generates books thumbnails

--- src/App.js
+++ src/App.js
@@ -1,6 +1,170 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import './App.css';
+import * as BooksAPI from './BooksAPI';
+
+class SearchField extends React.Component{
+  state = {
+    book : {  "book-title": '',
+              "book-authors" : '',
+              "book-cover" : ''      
+    }
+  }
+render(){
+  return( <div className="search-books">
+              <div className="search-books-bar">
+                <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+                <div className="search-books-input-wrapper">
+                  {/*
+                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                    You can find these search terms here:
+                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                    you don't find a specific author or title. Every search is limited by search terms.
+                  */}
+                  <input type="text" placeholder="Search by title or author"/>
+                </div>
+              </div>
+                <div className="search-books-results">
+                  <ol className="books-grid"></ol>
+              </div>
+          </div>
+  );
+  }
+}
+
+class BookShelfChanger extends React.Component{
+  render(){
+    return( <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+    );
+    }
+  }
+
+  class GenerateBook extends React.Component{
+
+    
+    render(){
+      
+      console.log(this.props.imageLinks.thumbnail);
+      return(
+                      <li>
+                        <div className="book">
+                            <div className="book-top">
+                            <div className="book-cover" 
+                              style={{ width: 128, height: 193, backgroundImage: `url("${this.props.imageLinks.thumbnail}")`}}></div>
+                              <BookShelfChanger></BookShelfChanger> 
+                              </div>                
+                            <div className="book-title">{this.props.bookTitle}</div>
+                            <div className="book-authors">{this.props.bookAuthors}</div>
+                        </div>
+                      </li>
+      )
+    }
+  }
+
+  class GenerateBooks extends React.Component{
+    constructor(props){
+      super(props);
+   //   this.state = {results: ['Waiting to fetch data', "Data soon ready"]};
+      this.state = {results: [
+        {
+            "title": "The Linux Command Line",
+            "subtitle": "A Complete Introduction",
+            "authors": [
+                "William E. Shotts, Jr."
+            ],
+            "publisher": "No Starch Press",
+            "publishedDate": "2012",
+            "description": "You've experienced the shiny, point-and-click surface of your Linux computer—now dive below and explore its depths with the power of the command line. The Linux Command Line takes you from your very first terminal keystrokes to writing full programs in Bash, the most popular Linux shell. Along the way you'll learn the timeless skills handed down by generations of gray-bearded, mouse-shunning gurus: file navigation, environment configuration, command chaining, pattern matching with regular expressions, and more. In addition to that practical knowledge, author William Shotts reveals the philosophy behind these tools and the rich heritage that your desktop Linux machine has inherited from Unix supercomputers of yore. As you make your way through the book's short, easily-digestible chapters, you'll learn how to: * Create and delete files, directories, and symlinks * Administer your system, including networking, package installation, and process management * Use standard input and output, redirection, and pipelines * Edit files with Vi, the world’s most popular text editor * Write shell scripts to automate common or boring tasks * Slice and dice text files with cut, paste, grep, patch, and sed Once you overcome your initial \"shell shock,\" you'll find that the command line is a natural and expressive way to communicate with your computer. Just don't be surprised if your mouse starts to gather dust. A featured resource in the Linux Foundation's \"Evolution of a SysAdmin\"",
+            "industryIdentifiers": [
+                {
+                    "type": "ISBN_13",
+                    "identifier": "9781593273897"
+                },
+                {
+                    "type": "ISBN_10",
+                    "identifier": "1593273894"
+                }
+            ],
+            "readingModes": {
+                "text": true,
+                "image": false
+            },
+            "pageCount": 480,
+            "printType": "BOOK",
+            "categories": [
+                "COMPUTERS"
+            ],
+            "averageRating": 4,
+            "ratingsCount": 2,
+            "maturityRating": "NOT_MATURE",
+            "allowAnonLogging": true,
+            "contentVersion": "1.2.2.0.preview.2",
+            "panelizationSummary": {
+                "containsEpubBubbles": false,
+                "containsImageBubbles": false
+            },
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+            },
+            "language": "en",
+            "previewLink": "http://books.google.com/books?id=nggnmAEACAAJ&dq=linux&hl=&cd=3&source=gbs_api",
+            "infoLink": "https://play.google.com/store/books/details?id=nggnmAEACAAJ&source=gbs_api",
+            "canonicalVolumeLink": "https://market.android.com/details?id=book-nggnmAEACAAJ",
+            "id": "nggnmAEACAAJ",
+            "shelf": "currentlyReading"
+        }]
+      }
+    }
+
+  componentDidMount(){
+    console.log ("componentWillMount");
+    let promise = BooksAPI.getAll();
+    let results = promise.then(res => {
+          this.setState({ results : res});
+          return res;
+    },(data) => {
+      return data;
+    }
+  );
+  }
+   render(){
+    let valuesArray = Object.values(this.state.results);
+      return(
+             <div className="book">
+                 {this.state.results.map(book => {
+                              return (<GenerateBook key={book.industryIdentifiers.identifier} bookTitle={book.title} bookAuthors={book.authors}
+                                                    imageLinks={book.imageLinks}></GenerateBook>
+                                     )
+                                }
+                        )
+                }
+              </div>
+            )
+      }
+}
+  class BookShelf extends React.Component{
+    render(){  
+      return(  <div className="bookshelf">
+      <h2 className="bookshelf-title">Currently Reading</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+        <GenerateBooks></GenerateBooks>
+        </ol>
+      </div>
+    </div>
+      );
+      }
+    }
 
 class BooksApp extends React.Component {
   state = {
@@ -10,6 +174,7 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+    shelf : '',
     showSearchPage: false
   }
 
@@ -17,26 +182,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+         <SearchField></SearchField>
         ) : (
           <div className="list-books">
             <div className="list-books-title">
@@ -44,49 +190,7 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">Ender's Game</div>
-                          <div className="book-authors">Orson Scott Card</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
+              <BookShelf> </BookShelf>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
@@ -180,7 +284,8 @@ class BooksApp extends React.Component {
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
-                                <option value="none">None</option>
+                                <option value="none">
+                                None</option>
                               </select>
                             </div>
                           </div>
